8f8abb7790939862b6d4751ec3d62437cb09ed51
Manners-Maketh-Man <junseo12154@naver.com>
2022-10-25 22:43:24 +0900

    가위, 바위, 보를 랜덤으로 선택하는 함수 소스파일 입니다.

---
+++ 가위바위보 랜덤.py
@@ -0,0 +1,22 @@
+def computer_random_choice():
+
+    import random
+
+    print("컴퓨터가 랜덤으로 가위, 바위, 보 중 하나를 선택합니다..")
+    computer = random.randrange(1,4)
+    """print(computer)"""           # 1, 2, 3 중 어떤 숫자가 뽑혔는지 확인
+    
+    if (computer == 1) :
+        computer = "가위"
+
+    elif (computer == 2) :
+        computer = "바위"
+
+    elif (computer == 3) :
+        computer = "보"
+
+    print("컴퓨터는",computer,"를 냈습니다.")
+    
+    return computer
+
+computer_random_choice()
